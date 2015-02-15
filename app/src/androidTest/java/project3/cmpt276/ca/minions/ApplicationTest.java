922b16d52aa590a4caf7add337d8d0619083cef2
Paul Madeya <pmadeya@sfu.ca>
2015-02-15 14:05:18 -0800

    Initial commit of project files

---
+++ app/src/androidTest/java/project3/cmpt276/ca/minions/ApplicationTest.java
@@ -0,0 +1,13 @@
+package project3.cmpt276.ca.minions;
+
+import android.app.Application;
+import android.test.ApplicationTestCase;
+
+/**
+ * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
+ */
+public class ApplicationTest extends ApplicationTestCase<Application> {
+    public ApplicationTest() {
+        super(Application.class);
+    }
+}
