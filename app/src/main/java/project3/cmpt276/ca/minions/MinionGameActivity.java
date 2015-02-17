108116babf7402fcaf9fe1ed3ccaf6c0b3cd9b3c
Arsalan Noroozzadeh <anoroozz@sfu.ca>
2015-02-17 15:24:01 -0800

    Animation files just added

---
+++ app/src/main/java/project3/cmpt276/ca/minions/MinionGameActivity.java
@@ -0,0 +1,50 @@
+package project3.cmpt276.ca.minions;
+
+import android.content.Intent;
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+
+
+public class MinionGameActivity extends MinionActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_minion_game);
+
+
+
+
+
+    }
+
+
+
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_minion_game, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
---
+++ app/src/main/java/project3/cmpt276/ca/minions/MinionMenuActivity.java
@@ -0,0 +1,83 @@
+package project3.cmpt276.ca.minions;
+
+import android.content.Intent;
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+
+
+public class MinionMenuActivity extends MinionActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_minion_menu);
+
+        chooseActivityFromMainMenu();
+
+    }
+
+
+
+    private void chooseActivityFromMainMenu() {
+
+        Button playGame = (Button) findViewById(R.id.btnPlayGame);
+        Button optionMenu = (Button) findViewById(R.id.btnOption);
+        Button helpMenu = (Button) findViewById(R.id.btnHelp);
+
+        playGame.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                startActivity(new Intent(MinionMenuActivity.this, MinionGameActivity.class));
+
+            }
+        });
+
+        optionMenu.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                startActivity(new Intent(MinionMenuActivity.this, MinionOptionActivity.class));
+
+            }
+        });
+
+        helpMenu.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MinionMenuActivity.this, MinionHelpActivity.class ));
+            }
+        });
+
+    }
+
+
+
+
+
+        @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_minion_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
