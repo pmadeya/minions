1793f34e852b1b28626d0b6f637d6124d17892ea
Arsalan Noroozzadeh <anoroozz@sfu.ca>
2015-02-25 16:04:47 -0800

    made Option menu landscape plus adding spinners

--- app/src/main/java/project3/cmpt276/ca/minions/MinionOptionActivity.java
+++ app/src/main/java/project3/cmpt276/ca/minions/MinionOptionActivity.java
@@ -6,17 +6,24 @@ import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Spinner;
+
 
 
 public class MinionOptionActivity extends MinionActivity {
+
+    private Spinner sp1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_minion_option);
-
+        makeBoardSpinnerBar();
+        addListenerToBoardSpinner();
 
 
 
@@ -24,24 +31,51 @@ public class MinionOptionActivity extends MinionActivity {
         doneBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                EditText rowNum = (EditText)findViewById(R.id.rowValue);
-                EditText colNum = (EditText)findViewById(R.id.colValue);
 
-                String rowNumString = rowNum.getText().toString();
-                int rowNumInteger = Integer.parseInt(rowNumString);
 
-                String colNumString = colNum.getText().toString();
-                int colNumInteger = Integer.parseInt(colNumString);
 
                 Intent intent = new Intent(MinionOptionActivity.this, MinionMenuActivity.class);
-                intent.putExtra("rowValueInt", rowNumInteger);
-                intent.putExtra("colValueInt", colNumInteger);
+                //intent.putExtra("rowValueInt", rowNumInteger);
+                //intent.putExtra("colValueInt", colNumInteger);
 
                 startActivity(intent);
             }
         });
 
 
+
+
+    }
+
+    private void makeBoardSpinnerBar(){
+        sp1 = (Spinner) findViewById(R.id.board_spinner);
+// Create an ArrayAdapter using the string array and a default spinner layout
+        ArrayAdapter<CharSequence> boardSizes = ArrayAdapter.createFromResource(this,
+                R.array.sizeOfBoard, android.R.layout.simple_spinner_item);
+// Specify the layout to use when the list of choices appears
+        boardSizes.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+// Apply the adapter to the spinner
+        sp1.setAdapter(boardSizes);
+
+    }
+
+
+    private void addListenerToBoardSpinner(){
+        sp1=(Spinner) findViewById(R.id.board_spinner);
+        sp1.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+
+                String itemU3=parent.getItemAtPosition(position).toString();
+
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+
+            }
+        });
 
 
     }
